e3ff5dd449e3701677beb7049273d164d629212b
binhlaig <binhlaig@gmail.com>
2025-04-16 19:59:06 +0900

    setup app, file and folder

---
+++ Types/index.d.ts
@@ -0,0 +1,2 @@
+
+type FormType = "sign-in" | "sign-up";
---
+++ components/AuthForm.tsx
@@ -0,0 +1,104 @@
+"use client"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import FormField from "./FormField"
+import { Form } from "@/components/ui/form";
+import Link from "next/link"
+
+
+
+
+
+const AuthForm = ({ type }: { type: FormType }) => {
+
+    const formSchema = z.object({
+        name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(3),
+    })
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+    const isSignIn = type === "sign-in";
+
+    return (
+        <div className="border-gradient p-0.5 rounded-2xl w-fit lg:min-w-[556px]">
+            <div className="flex flex-col gap-6 card py-14 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                    <h2 className="text-primary-100">PrepWise</h2>
+                </div>
+
+                <h3>Practice job interviews with AI</h3>
+
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="w-full space-y-6 mt-4 form"
+                    >
+                        {!isSignIn && (
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                label="Name"
+                                placeholder="Your Name"
+                                type="text"
+                            />
+                        )}
+
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            label="Email"
+                            placeholder="Your email address"
+                            type="email"
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            label="Password"
+                            placeholder="Enter your password"
+                            type="password"
+                        />
+
+                        <Button className="btn" type="submit">
+                            {isSignIn ? "Sign In" : "Create an Account"}
+                        </Button>
+                    </form>
+                </Form>
+
+                <p className="text-center">
+                    {isSignIn ? "No account yet?" : "Have an account already?"}
+                    <Link
+                        href={!isSignIn ? "/sign-in" : "/sign-up"}
+                        className="font-bold text-user-primary ml-1"
+                    >
+                        {!isSignIn ? "Sign In" : "Sign Up"}
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
+
+}
+
+export default AuthForm
